7a12c2a3807e478619a148260b9e1d4e1211bee2
Nouman Rafi <mnrmogul9785@gmail.com>
2023-11-20 21:11:08 +0500

    init task3.1

--- task3/middleware.py
+++ task3/middleware.py
@@ -1,5 +1,7 @@
 import logging
 
+from django.utils import timezone
+
 logger = logging.getLogger(__name__)
 
 
@@ -15,3 +17,35 @@ class CustomAuthMiddleware:
 
         response = self.get_response(request)
         return response
+
+
+class RequestLogMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get user IP and request data
+        ip_add = self.get_user_ip(request)
+        method = request.method
+        url = request.build_absolute_uri()
+
+        # Log the data
+        log_info = f"[{timezone.now()}] IP: {ip_add}\
+              | Method: {method} | URL: {url}\n"
+        self.do_log(log_info)
+
+        # Continue request
+        response = self.get_response(request)
+        return response
+
+    def get_user_ip(self, request):
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(",")[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+        return ip
+
+    def do_log(self, log_info):
+        with open("req_info_log.txt", "a") as log_file:
+            log_file.write(log_info)
