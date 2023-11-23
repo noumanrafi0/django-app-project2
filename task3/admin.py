6898dffe2b875c5b391a0b78e1c1fb1f9e805cba
Nouman Rafi <mnrmogul9785@gmail.com>
2023-11-23 14:01:06 +0500

    imp task3

--- task3/admin.py
+++ task3/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .models import CustomUser
 
 
-class AuthorAdmin(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
     pass
 
 
-admin.site.register(CustomUser, AuthorAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- task3/middleware.py
+++ task3/middleware.py
@@ -1,4 +1,8 @@
 import logging
+from django.http import HttpResponse
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
 
 logger = logging.getLogger(__name__)
 
@@ -8,10 +12,17 @@ class CustomAuthMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not request.user.is_authenticated:
-            logger.info("not authenticated")
-        else:
-            logger.info("authenticated")
+        if not request.user.is_authenticated and request.method == "POST":
+            phone_number = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(username=phone_number, password=password)
+
+            if user is not None:
+                request.user = user
+            else:
+                return HttpResponse(
+                    "<h1 style='color: red; text-align: center;'>Access Denied</h1>"
+                )
 
         response = self.get_response(request)
         return response
